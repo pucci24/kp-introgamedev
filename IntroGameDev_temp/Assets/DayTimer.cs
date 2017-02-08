1c97e4f201ca106a19790a49dcdf889af187b7c1
Keanan Pucci <kcp320@nyu.edu>
2017-02-08 10:50:27 -0500

    Week 1 Sprint

---
+++ IntroGameDev_temp/Assets/DayTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayTimer : MonoBehaviour {
+	int day=1;
+	float timePassed=0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timePassed += Time.deltaTime;
+		if (timePassed >= 300f) {
+			day += 1;
+			timePassed = 0f;
+		}
+			
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(145, 15, 100, 20), "Day: "+day);
+	}
+}
---
+++ IntroGameDev_temp/Assets/HoverButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverButton : MonoBehaviour {
+
+	bool hover = false;
+	public GameObject Option1;
+	public GameObject Option2;
+	public GameObject Option3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hover == true) {
+			if (Option1.transform.position.x < -4.05) {
+				Option1.transform.Translate (0.1f, 0f, 0f);
+			} else if (Option1.transform.position.x >= -4.05) {
+				hover = false;
+			}
+			if (Option2.transform.position.x < -2.85) {
+				Option2.transform.Translate (0.1f, 0f, 0f);
+			}
+			else if (Option2.transform.position.x >= -2.85) {
+				hover = false;
+			}
+			if (Option3.transform.position.x < -1.65) {
+				Option3.transform.Translate (0.1f, 0f, 0f);
+			}else if (Option3.transform.position.x >= -1.65) {
+				hover = false;
+			}
+		}
+	}
+
+
+	void OnMouseEnter()
+	{
+		hover = true;
+	}
+
+}
